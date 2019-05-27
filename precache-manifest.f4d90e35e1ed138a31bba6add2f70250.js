b4df69615f4471e52c235eafb046552f5490ed91
Kainar <kainar.ilyasov@gmail.com>
2019-05-27 12:45:25 +0600

    Updates

---
+++ precache-manifest.f4d90e35e1ed138a31bba6add2f70250.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "293a3cadf652db67f739",
+    "url": "/gitsearch/static/js/runtime~main.810e9d08.js"
+  },
+  {
+    "revision": "4ee3edab57c387fae2a5",
+    "url": "/gitsearch/static/js/main.365b201b.chunk.js"
+  },
+  {
+    "revision": "6d63310ec8a70c59c587",
+    "url": "/gitsearch/static/js/2.691e3f9f.chunk.js"
+  },
+  {
+    "revision": "4ee3edab57c387fae2a5",
+    "url": "/gitsearch/static/css/main.2563e3c0.chunk.css"
+  },
+  {
+    "revision": "b7e655ff0dc8ea038689cf3bff6488af",
+    "url": "/gitsearch/index.html"
+  }
+];
